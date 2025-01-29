783a445f1325ad0652f60eb8a3dc0b45db15302b
fuadul hasan <53356319+fuad7161@users.noreply.github.com>
2025-01-30 00:21:04 +0600

    Add files via upload

---
+++ token/maker.go
@@ -0,0 +1,10 @@
+package token
+
+import "time"
+
+// Maker is an interface for managing tokens
+type Maker interface {
+	CreateToken(username string, role string, duration time.Duration) (string, *Payload, error)
+
+	VerifyToken(token string) (*Payload, error)
+}
